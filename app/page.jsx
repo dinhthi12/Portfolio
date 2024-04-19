f6e047d3bcc402253f6207d37064b12b7ea5a23e
dinhthi12 <thi884641@gmail.com>
2024-04-19 15:25:01 +0700

    Add services

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,14 @@
 import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Image from "next/image";
+import Service from "@/components/Service";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <About />
+      <Service />
     </main>
   );
 }
